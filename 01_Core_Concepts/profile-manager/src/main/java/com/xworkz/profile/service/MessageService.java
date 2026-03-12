a57a222db2bce75a78b04d84d402d3a2c712df17
C Mallikarjun <chapisgarmallikarjun@gmail.com>
2026-03-12 19:41:19 +0530

    Delete 01_Core_Concepts directory

--- 01_Core_Concepts/profile-manager/src/main/java/com/xworkz/profile/service/MessageService.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.profile.service;
-
-// Interface to define the contract for profile-related messaging services
-public interface MessageService {
-
-    // Abstract method to return a standard greeting or welcome string
-    String getWelcomeMessage();
-}
